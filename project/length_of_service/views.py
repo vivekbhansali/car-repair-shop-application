8f9de113e1b35b9550e4472b901b4df9e14459f5
root <root@precise32.(none)>
2017-02-16 03:26:12 +0000

    Refactoring view.py and adding new file calc.py

--- project/length_of_service/views.py
+++ project/length_of_service/views.py
@@ -1,37 +1,23 @@
 from django.shortcuts import render
-from datetime import datetime
 from django.http import HttpResponse
-from .models import Workflow, RepairTypes
-from collections import defaultdict
 from django.template import loader
 
-def calculate_length_of_service(request):
+from .models import Workflow, RepairTypes
+import calc
+
+def length_of_service(request):
+	result = []
+        n_average_records = {}
+
 	mechanics =  Workflow.objects.values_list('mechanic', flat=True).distinct()
 	types = RepairTypes.objects.values_list('repair_type', flat=True)
 	national_averages = RepairTypes.objects.values_list('national_averages', flat=True)
-	result = []
-	n_average_records = {}
+	services = Workflow.objects.all()
 	for rtype, avg in zip(types, national_averages):
 		n_average_records[rtype] = avg
-	services = Workflow.objects.all()
-	for mechanic in mechanics:
-		mechanic = mechanic.strip()
-		sum_records = defaultdict(int)
-		count_records = defaultdict(int)
-		for service in services:
-			s = str(service).split(",")
-			name = s[2].strip()
-			if name == mechanic:
-				repairType = s[3].strip()
-				sum_records[repairType] += (abs(datetime.strptime(s[0], '%m/%d/%Y') - datetime.strptime(s[1], '%m/%d/%Y')).days + 1)
-				count_records[repairType] += 1
-		
-		for types in sum_records:
-			avg = sum_records[types]/count_records[types]
-			ratio_of_avgs = round(float(n_average_records[types]/avg),1)
-			m = str(mechanic + ' - ' + types + ' - ' + str(avg) + ' - ' + str(ratio_of_avgs))
-			result.append(m)
 
+	result = calc.calculate_length_of_service(mechanics, services, types, n_average_records)
+	
 	template = loader.get_template('length_of_service/index.html')
 	context = {'result_list': result,}
 	return HttpResponse(template.render(context, request))
