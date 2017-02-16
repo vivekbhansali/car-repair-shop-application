cc762e76fc197dbb4d7cca8e22e6aed901eb6a86
root <root@precise32.(none)>
2017-02-16 11:03:50 +0000

    Adding new feature - sort length of services by repair type

--- project/length_of_service/calc.py
+++ project/length_of_service/calc.py
@@ -5,7 +5,7 @@ from .models import Workflow, RepairTypes
 from collections import defaultdict
 from django.template import loader
 
-def calculate_length_of_service(mechanics, services, n_average_records):
+def calculate_length_of_service_by_mechanic(mechanics, services, n_average_records):
         result = []
 	for mechanic in mechanics:
                 mechanic = mechanic.strip()
@@ -26,3 +26,27 @@ def calculate_length_of_service(mechanics, services, n_average_records):
                         result.append(m)
 
         return result
+
+def calculate_length_of_service_by_repair_type(types, services, n_average_records):
+        result = []
+        for repair_type in types:
+                repair_type = repair_type.strip()
+		mechanics = []
+                sum_records = defaultdict(int)
+                count_records = defaultdict(int)
+                for service in services:
+                        s = str(service).split(",")
+                        repair = s[3].strip()
+                        if repair == repair_type:
+                                mechanic = s[2].strip()
+				mechanics.append(mechanic)
+                                sum_records[mechanic] += (abs(datetime.strptime(s[0], '%m/%d/%Y') - datetime.strptime(s[1], '%m/%d/%Y')).days + 1)
+                                count_records[mechanic] += 1
+
+                for mechanic in sum_records:
+                        avg = sum_records[mechanic]/count_records[mechanic]
+                        ratio_of_avgs = round(float(float(n_average_records[repair_type]/avg)),1)
+                        m = str(mechanic + ' - ' + repair_type + ' - ' + str(avg) + ' - ' + str(ratio_of_avgs))
+                        result.append(m)
+        
+	return result
--- project/length_of_service/views.py
+++ project/length_of_service/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 from .models import Workflow, RepairTypes
 import calc
 
-def length_of_service(request):
+def length_of_service_by_mechanic(request):
 	result = []
         n_average_records = {}
 
@@ -16,11 +16,28 @@ def length_of_service(request):
 	for rtype, avg in zip(types, national_averages):
 		n_average_records[rtype] = avg
 
-	result = calc.calculate_length_of_service(mechanics, services, n_average_records)
+	result = calc.calculate_length_of_service_by_mechanic(mechanics, services, n_average_records)
 	
-	template = loader.get_template('length_of_service/index.html')
+	template = loader.get_template('length_of_service/display_by_mechanic.html')
 	context = {'result_list': result,}
 	return HttpResponse(template.render(context, request))
 
+def length_of_service_by_repair_type(request):
+        result = []
+        n_average_records = {}
+
+        mechanics =  Workflow.objects.values_list('mechanic', flat=True).distinct()
+        types = RepairTypes.objects.values_list('repair_type', flat=True)
+        national_averages = RepairTypes.objects.values_list('national_averages', flat=True)
+        services = Workflow.objects.all()
+        for rtype, avg in zip(types, national_averages):
+                n_average_records[rtype] = avg
+
+        result = calc.calculate_length_of_service_by_repair_type(types, services, n_average_records)
+        
+	template = loader.get_template('length_of_service/display_by_repair_type.html')
+        context = {'result_list': result,}
+        return HttpResponse(template.render(context, request))
+
 def index(request):
-	return HttpResponse('Welcome to LOS Application!')
+	return HttpResponse('Welcome to \'FOO BAR\' Car Repair Shop - Length of Service Application!')
